cfa6f0868b184d17ac4a0fda122a9d87a6b5b1a4
Noah Wisner <112119476+Noah-Wisner@users.noreply.github.com>
2026-04-30 08:30:22 +0000

    baud was 7.37 instead of 7.38

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@ int cs122a_main() {
     UART *up = &uart[i];
     unsigned int divisor = *(up->base + UARTIBRD);
     unsigned int lcr = *(up->base + UARTLCR);
-    unsigned int baud = 7370000 / (16 * divisor);
+    unsigned int baud = 7380000 / (16 * divisor);
 
     uprintf(&uart[0], "UART[%d] Baudrate: %u\n", i, baud);
     uprintf(&uart[0], "UART[%d] Line Control Register: 0x%x\n", i, lcr);
